d38f7f71b3ae57ed18bfc4a8df1acb3781ef7122
dgpe.curso.03@gmail.com <dgpe.curso.03@gmail.com>
2018-06-07 18:28:05 -0500

    cambios 5

--- src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -10,15 +10,45 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
     
-    //@Test
-    public void ok() throws Exception {
-        //String result = sendGet("https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-        
-	//String result = sendGet("http://localhost:9090/api/sumar?a=78&b=29");
-        //assertTrue("Operacion suma", result.length() >1);
-        assertTrue(true);
+    @Test
+    public void suma() throws Exception {
 
-	//logger.info(result);
+        
+	String result = sendGet("http://localhost:8080/api/sumar?a=11&b=14");
+        System.out.println(result);
+        assertTrue("Operacion suma", result.length() > 1);
+        
+
+    }
+
+    @Test
+    public void restar() throws Exception {
+       
+	String result = sendGet("http://localhost:8080/api/restar?a=11&b=14");
+        System.out.println(result);
+        assertTrue("Operacion restar", result.length() > 1);
+        
+
+    }
+
+    @Test
+    public void dividir() throws Exception {
+       
+	String result = sendGet("http://localhost:8080/api/divide?a=11&b=14");
+        System.out.println(result);
+        assertTrue("Operacion dividir", result.length() > 1);
+        
+
+    }
+
+
+    @Test
+    public void multiplicar() throws Exception {
+       
+	String result = sendGet("http://localhost:8080/api/multiplica?a=11&b=14");
+        System.out.println(result);
+        assertTrue("Operacion multiplicar", result.length() > 1);
+        
 
     }
 
